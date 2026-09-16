ea9943de99a84ec81b8dfc263b4a954d01e88d9a
brycehaley620 <bryce.haley10@okstate.edu>
2026-09-15 20:56:42 -0500

    Add Docker build and publish commands

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,5 +1,10 @@
 # Question 1
-
+# Question 1
+docker build -t linear-regression-app .
+docker tag linear-regression-app linear-regression-app:v0.1.1
+docker tag linear-regression-app linear-regression-app:v0.1.2
+docker tag linear-regression-app brycehaley/linear-regression-app:latest
+docker push brycehaley/linear-regression-app:latest
 
 # Question 2
 
