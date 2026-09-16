3edc4aa068f5d6ea59970184c60a697bf3796938
brycehaley620 <bryce.haley10@okstate.edu>
2026-09-15 21:14:35 -0500

    Update Docker commands

--- docker commands.sh
+++ docker commands.sh
@@ -1,17 +1,17 @@
 # Question 1
 
-cd assignment1/regression_container
-docker build -t linear-regression-app:latest .
-docker tag linear-regression-app:latest linear-regression-app:v0.1.1
-docker tag linear-regression-app:latest linear-regression-app:v0.1.2
-docker tag linear-regression-app:latest brycehaley/linear-regression-app:latest
+docker build -t linear-regression-app .
+docker tag linear-regression-app linear-regression-app:v0.1.1
+docker tag linear-regression-app linear-regression-app:v0.1.2
+docker tag linear-regression-app brycehaley/linear-regression-app:latest
 docker push brycehaley/linear-regression-app:latest
 
 # Question 2
 
-cd ../volume-mounting
-docker build -t volume-mounting-app:latest .
-docker run --rm --volume "${PWD}/data:/home/iem4723/data" volume-mounting-app:latest
+docker build -t volume-mounting-app .
+docker run --volume "${PWD}/data:/home/iem4723/data" volume-mounting-app
 
 # Question 3
+
+cd ..
 # Question 4
